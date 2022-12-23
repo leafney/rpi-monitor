71d0dd7824e98349bf16cd22746cc33d51f40567
leafney <babycoolzx@126.com>
2022-12-24 03:03:50 +0800

    add get basic info

---
+++ module/basic.go
@@ -0,0 +1,45 @@
+/**
+ * @Author:      leafney
+ * @Date:        2022-12-24 02:26
+ * @Project:     rpi-monitor
+ * @HomePage:    https://github.com/leafney
+ * @Description:
+ */
+
+package module
+
+import (
+	"github.com/leafney/rpi-monitor/utils"
+	"strings"
+	"time"
+)
+
+// GetBasicDateTime Get the current time of the system
+func GetBasicDateTime() string {
+	res, _ := utils.RunCommand(`/usr/bin/date "+%Y-%m-%d %H:%M:%S"`)
+	return res
+}
+
+// GetBasicBootTime Get system boot time
+func GetBasicBootTime() string {
+	res, _ := utils.RunCommand("/usr/bin/uptime -s")
+	return res
+}
+
+// GetBasicDeviceModel Get the system device model
+func GetBasicDeviceModel() string {
+	res, _ := utils.RunCommand(`/bin/cat /proc/device-tree/model`)
+
+	// Raspberry Pi 3 Model B Plus Rev 1.3 ==> RPi 3 Model B+ r1.3
+	strings.ReplaceAll(res, "Raspberry ", "R")
+	strings.ReplaceAll(res, "i Model ", "i 1 Model")
+	strings.ReplaceAll(res, " Plus", "+")
+	strings.ReplaceAll(res, "Rev ", "r")
+
+	return res
+}
+
+func GetBasicTimestamp() string {
+	res := time.Now().Format(time.RFC3339)
+	return res
+}
