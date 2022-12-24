43ec36ac9f77d75304b0246f58c07ffc5189391d
leafney <babycoolzx@126.com>
2022-12-24 21:17:28 +0800

    test os info

--- main.go
+++ main.go
@@ -7,19 +7,32 @@ import (
 
 func main() {
 	//	test basic info
+	/*
+		fmt.Println("boot_time ", module.GetBasicBootTime())
+		fmt.Println("date_time ", module.GetBasicDateTime())
+		fmt.Println("timestamp ", module.GetBasicTimestamp())
+		fmt.Println("device_mode ", module.GetBasicDeviceModel())
+		upTime := module.GetBasicUpTime()
+		fmt.Println("uptime ", upTime)
+		utd, uth, utm := module.GetBasicUpTimeDHM(upTime)
+		fmt.Printf("uptime_days [%s] uptime_hours [%s] uptime_mins [%s] \n", utd, uth, utm)
+		//boot_time  2022-11-24 03:13:09
+		//date_time  2022-12-24 18:04:49
+		//timestamp  2022-12-24T18:04:49+08:00
+		//device_mode  RPi 3 Model B+ r1.3
+		//uptime  30 days, 14 hours, 51 mins
+		//uptime_days [30] uptime_hours [14] uptime_mins [51]
 
-	fmt.Println("boot_time ", module.GetBasicBootTime())
-	fmt.Println("date_time ", module.GetBasicDateTime())
-	fmt.Println("timestamp ", module.GetBasicTimestamp())
-	fmt.Println("device_mode ", module.GetBasicDeviceModel())
-	upTime := module.GetBasicUpTime()
-	fmt.Println("uptime ", upTime)
-	utd, uth, utm := module.GetBasicUpTimeDHM(upTime)
-	fmt.Printf("uptime_days [%s] uptime_hours [%s] uptime_mins [%s] \n", utd, uth, utm)
-	//boot_time  2022-11-24 03:13:09
-	//date_time  2022-12-24 18:04:49
-	//timestamp  2022-12-24T18:04:49+08:00
-	//device_mode  RPi 3 Model B+ r1.3
-	//uptime  30 days, 14 hours, 51 mins
-	//uptime_days [30] uptime_hours [14] uptime_mins [51]
+	*/
+
+	//	test os info
+	fmt.Println(module.GetOSHostName(""))
+	fmt.Println(module.GetOSKernel())
+	fmt.Println(module.GetOSDistro())
+	fmt.Println(module.GetOSCodeName())
+	//raspberrypi raspberrypi
+	//Linux 5.10.17-v7+
+	//Raspbian GNU/Linux 10 (buster)
+	//buster
+
 }
